5e25c37e5425cae827c213a122c10e25e4e73640
1amrishi <120283735+1amrishi@users.noreply.github.com>
2025-06-06 02:17:11 +0530

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -132,7 +132,7 @@ export const RESUME_DATA = {
         "Product Marketing",
         "Bootstrapped",
       ],
-      description: "Founder and CEO of Bombay Print Lab, a bootstrapped 3D printing service in Mumbai. Successfully scaled operations to 3 machines and achieved 3+ lakhs in annual revenue",
+      description: "Founder and CEO of Bombay Print Lab, a bootstrapped 3D printing service in Mumbai. Successfully scaled operations to 5 machines and achieved 10+ lakhs in annual revenue",
       logo: ConsultlyLogo,
       link: {
         label: "https://bombayprintlab.com",
